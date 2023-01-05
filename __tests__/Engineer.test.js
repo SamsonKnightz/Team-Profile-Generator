74caf3d28660032d09952e2d177a1096e7e37340
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2023-01-04 21:22:20 -0500

    added ID and Email cards

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,44 +2,23 @@ const Employee = require("../lib/Employee");
 const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
-
-});
-const Engineer = require("../lib/Engineer");
-
-describe("Engineer", () => {
   describe("name", () => {
     it("returns a name", () => {
-      const employee = new Employee("Samson", )
-      const result = new Algo().reverse(str);
+      const employee = new Employee("Samson", 1234, "samson.v@tesla.com")
 
-      expect(result).toEqual(reversed);
     });
   });
 
   describe("ID", () => {
     it("returns the ID number", () => {
       const employee = new employee("Samson", 1234, "samson.v@tesla.com");
-
-      expect(result).toEqual(true);
     });
 
-    it("should return false if a string is not a palindrome", () => {
-      const str = "neon";
-
-      const result = new Algo().isPalindrome(str);
-
-      expect(result).toEqual(false);
-    });
   });
 
-  describe("capitalize", () => {
-    it("should take a string and return a new string with the first letter of each word capitalized", () => {
-      const str = "capitalize every first word of the string.";
-      const capitalized = "Capitalize Every First Word Of The String.";
-
-      const result = new Algo().capitalize(str);
-
-      expect(result).toEqual(capitalized);
+  describe("Email", () => {
+    it("returns the Email", () => {
+        const employee = new employee("Samson", 1234, "samson.v@tesla.com");
     });
   });
 });
